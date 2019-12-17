614dd67774c91fe8e394283c614314c43f6badf7
Dionuta <diounta@gmail.com>
2019-12-16 16:31:28 -0800

    PUSH

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,17 @@
 
 ## Exercise I
 
-a)
+a) O(n) 
 
 
 b)
-
+  O(nlog(n))) j in the inner loops keeps getting bigger every time the function runs
 
 c)
+O(n) since we subtract by on the function is only being call n times 
 
 ## Exercise II
+
+    I would just start at the halfway point and test if it breaks, if it does I would eliminate the top half and keep doing this until I find a point where it does not. This call binary search.
 
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,19 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+     
+    sub_word="th"
+    # if the word length is zero return 0
+    if len(word) == 0: 
+        return 0
+
+    # starts the at the beginning of word check if subword is at the start
+    # if so count goes up then we move a space over 
+
+    elif word[0 : len(sub_word)] == sub_word: 
+        return 1 + count_th(word[1:]) 
+
+    # keep checking till base case is reached 
+    return count_th(word[1:])
     
-    # TBC
-    
-    pass
+print(count_th("mjthsxth"))
